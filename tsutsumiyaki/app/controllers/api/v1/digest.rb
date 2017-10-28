93aaa5dcdc4f570c92bbed6616d7bbc97c96faa0
takumibv <everybody.say.hey.yo@gmail.com>
2017-10-28 17:35:17 +0900

    要約API・地図API

--- tsutsumiyaki/app/controllers/api/v1/digest.rb
+++ tsutsumiyaki/app/controllers/api/v1/digest.rb
@@ -1,25 +1,39 @@
 # ChannelsController
 # require 'utils/error.rb'
+require 'net/https'
 module Api
   module V1
     class Digest < ApiController
-      API_URL = ""
+      API_URL = "http://www.47news.jp/cgi-bin/ra/meta_ex.cgi"
 
       def self.geta
         return "OK"
       end
 
       def self.fetchAllChannels
-      request_url = API_URL + "v4/home.php"
-      params = URI.encode_www_form({ param1: '111', param2: '222' })
-      uri = URI.parse("#{request_url}?#{params}")
+      request_url = API_URL
+      params = URI.encode_www_form(
+          { 'title' => '早大３連覇　斎藤が１５奪三振、初完封　東京六大学野球',
+            'body' => '東京六大学野球秋季リーグは３０日、神宮球場で最終週の早大—慶大３回戦があり、早大が斎藤（１年、早稲田実）の活躍で慶大に７—０で大勝し、３季連続４０度目の優勝を果たした。勝ち点４で明大と並んだが、勝率で上回った。早大は１１月１０日開幕の明治神宮大会への出場も決めた。斎藤はスライダーやツーシームなどの変化球がさえ、リーグ戦初完封。被安打４で１５奪三振の力投で今季４勝目を挙げた。打線は１回、松本（３年、千葉経大付）の適時打と本田（４年、智弁和歌山）の３点二塁打で４点を先取し、その後も加点した。慶大は３連投のエース加藤幹（４年、川和）が力尽きた。',
+            'enc'  => 'utf-8', # 入出力文字コードがUTF-8
+            'debug' => '1', # 0:false 1:true
+            'permax' => '50' #要約圧縮率
+      })
+      uri = URI.parse("#{request_url}")
 
       begin
-        response = Net::HTTP.start(uri.host, uri.port) do |http|
-          http.open_timeout = 5
-          http.read_timeout = 10
-          http.get(uri.request_uri)
-        end
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        req = Net::HTTP::Post.new(uri.path)
+        req.set_form_data({ 'title' => '早大３連覇　斎藤が１５奪三振、初完封　東京六大学野球',
+                            'body' => '東京六大学野球秋季リーグは３０日、神宮球場で最終週の早大—慶大３回戦があり、早大が斎藤（１年、早稲田実）の活躍で慶大に７—０で大勝し、３季連続４０度目の優勝を果たした。勝ち点４で明大と並んだが、勝率で上回った。早大は１１月１０日開幕の明治神宮大会への出場も決めた。斎藤はスライダーやツーシームなどの変化球がさえ、リーグ戦初完封。被安打４で１５奪三振の力投で今季４勝目を挙げた。打線は１回、松本（３年、千葉経大付）の適時打と本田（４年、智弁和歌山）の３点二塁打で４点を先取し、その後も加点した。慶大は３連投のエース加藤幹（４年、川和）が力尽きた。',
+                            'enc'  => 'utf-8', # 入出力文字コードがUTF-8
+                            'debug' => '1', # 0:false 1:true
+                            'permax' => '50' #要約圧縮率
+                          })
+
+        response = http.request(req)
 
         # [レスポンス処理]
         # 1xx系 => Net::HTTPInformation
